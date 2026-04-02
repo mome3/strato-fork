b14dd73edcf058b7b161f327be24159444294c3a
mo-mew <handsaw-mowing-8l@icloud.com>
2026-04-02 14:46:07 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,8 +38,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: '#f9f9f9',
   viewportFit: 'cover',
-  maximumScale: 1,
-  userScalable: false,
+  width: 'device-width',
+  initialScale: 1,
 }
 
 export default function RootLayout({
